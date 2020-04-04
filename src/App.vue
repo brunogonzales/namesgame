4f5d8c6c2ab949b68a654d68ec0f41bf95df5ff9
Bruno Gonzales <brunogonzales1129@gmail.com>
2020-04-04 13:54:54 -0500

    Add seed questions and basic setup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="gameEnded">
+      <h1>Oops! Se nos acabaron las preguntas...</h1>
+      <a
+        href="https://github.com/brunogonzales/namesgame"
+      >Envia tus preguntas al repositorio en GitHub</a>
+    </div>
+    <div v-else>
+      <button @click="nextSlide">Siguiente</button>
+      <h2>{{currentPlayer}}</h2>
+      <h4>{{currentQuestion}}</h4>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { questions } from "./constants";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      players: [],
+      currentPlayer: "",
+      currentQuestion: "",
+      questions
+    };
+  },
+  created() {
+    this.setPlayers();
+    this.nextSlide();
+  },
+  methods: {
+    setPlayers() {
+      this.players = prompt(
+        "Ingresa los nombres de los players separados por una coma"
+      ).split(",");
+    },
+
+    nextSlide() {
+      if (!this.questions.length) {
+        this.gameEnded = true;
+      } else {
+        const randomQuestionIndex = this.randomNumber(this.questions.length);
+        this.currentPlayer = this.players[
+          this.randomNumber(this.players.length)
+        ];
+        this.currentQuestion = this.questions[randomQuestionIndex];
+        this.questions = [
+          ...this.questions.slice(0, randomQuestionIndex),
+          ...this.questions.slice(randomQuestionIndex + 1)
+        ];
+      }
+    },
+
+    randomNumber(max) {
+      return Math.floor(Math.random() * max);
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 15px;
 }
 </style>
